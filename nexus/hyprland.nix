d41d5f4b617eb9546465649af181e8aa2c1dd7cb
Entwicklerfuchs26 <jonas@hofpause.info>
2026-05-13 18:31:23 +0200

    sddm runCommand patch

--- nexus/hyprland.nix
+++ nexus/hyprland.nix
@@ -1,25 +1,28 @@
 { config, pkgs,quickshell,awww, ... }:
 
 let
-  sddm-theme = pkgs.sddm-astronaut.override {
-    themeConfig = {
-      Background = "Backgrounds/astronaut.png";
-      PartialBlur = "true";
-      DimBackground = "0.3";
-      FormPosition = "center";
-      HourFormat = "HH:mm";
-      DateFormat = "dddd d. MMMM";
-      Font = "JetBrains Mono";
-      RoundCorners = "20";
-      ForceLastUser = "true";
-      PasswordFocus = "true";
-      HideCompletePassword = "true";
-      TranslateLogin = "Anmelden";
-      TranslateReboot = "Neustart";
-      TranslateShutdown = "Herunterfahren";
-      TranslateSuspend = "Ruhezustand";
-    };
-  };
+  sddm-theme = pkgs.runCommand "sddm-astronaut-patched" {} ''
+    cp -r ${pkgs.sddm-astronaut}/share/sddm/themes/sddm-astronaut-theme $out
+    chmod -R +w $out
+    cat > $out/Themes/astronaut.conf << 'EOF'
+    [General]
+    Background=Backgrounds/astronaut.png
+    DimBackground=0.3
+    PartialBlur=true
+    FormPosition=center
+    HourFormat=HH:mm
+    DateFormat=dddd d. MMMM
+    Font=JetBrains Mono
+    RoundCorners=20
+    ForceLastUser=true
+    PasswordFocus=true
+    HideCompletePassword=true
+    TranslateLogin=Anmelden
+    TranslateReboot=Neustart
+    TranslateShutdown=Herunterfahren
+    TranslateSuspend=Ruhezustand
+    EOF
+  '';
 in
 
 {
