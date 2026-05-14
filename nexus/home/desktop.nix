134e6bd291f36b23cb432c3ac45fec01a128d9de
Entwicklerfuchs26 <jonas@hofpause.info>
2026-05-14 22:52:30 +0200

    openrgb config in home manager

--- nexus/home/desktop.nix
+++ nexus/home/desktop.nix
@@ -4,4 +4,5 @@
   home.file.".config/hypr/hypridle.conf".source = ./files/hypridle.conf;
   home.file.".config/wlogout/style.css.templ".source = ./files/wlogout-style.css.templ;
   home.file.".config/nwg-dock-hyprland/style.css.templ".source = ./files/nwg-dock-style.css.templ;
+  home.file.".config/OpenRGB/OpenRGB.json".source = ./files/OpenRGB.json;
 }
