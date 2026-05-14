1cb6853573b8220d44fab18a630e220fcb31ff70
Entwicklerfuchs26 <jonas@hofpause.info>
2026-05-14 22:54:44 +0200

    gtk and cursor in home manager

--- nexus/home/desktop.nix
+++ nexus/home/desktop.nix
@@ -5,4 +5,7 @@
   home.file.".config/wlogout/style.css.templ".source = ./files/wlogout-style.css.templ;
   home.file.".config/nwg-dock-hyprland/style.css.templ".source = ./files/nwg-dock-style.css.templ;
   home.file.".config/OpenRGB/OpenRGB.json".source = ./files/OpenRGB.json;
+  home.file.".config/gtk-3.0/settings.ini".source = ./files/gtk3-settings.ini;
+  home.file.".config/gtk-4.0/settings.ini".source = ./files/gtk4-settings.ini;
+  home.file.".icons/default/index.theme".source = ./files/cursor-index.theme;
 }
