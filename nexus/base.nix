bef48ae2fc4507f6c7d6665312d395f26fac3a41
Entwicklerfuchs26 <jonas@hofpause.info>
2026-06-10 16:28:39 +0200

    base: udisks2 + gvfs + ntfs/exfat + polkit für Dolphin-Mounting

--- nexus/base.nix
+++ nexus/base.nix
@@ -70,4 +70,21 @@ services.udev.extraRules = ''
   KERNEL=="i2c-[0-9]*", GROUP="i2c", MODE="0660"
 '';
 
+# Festplatten-Erkennung & Mounting für Dolphin
+  services.udisks2.enable = true;
+  services.gvfs.enable = true;
+  boot.supportedFilesystems = [ "ntfs" "exfat" ];
+  environment.systemPackages = with pkgs; [ udisks2 ];
+
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+      if (
+        subject.isInGroup("wheel") &&
+        action.id.indexOf("org.freedesktop.udisks2.") == 0
+      ) {
+        return polkit.Result.YES;
+      }
+    });
+  '';
+
 }
